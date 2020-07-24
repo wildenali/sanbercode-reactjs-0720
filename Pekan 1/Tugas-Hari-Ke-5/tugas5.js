ae52b69c8313b5cf43a895a26110bf7fee700528
wildenali <wildenwildenaliali@gmail.com>
2020-07-24 21:08:28 +0700

    Tugas ke 5, pekan ke 1

---
+++ Pekan 1/Tugas-Hari-Ke-5/tugas5.js
@@ -0,0 +1,30 @@
+// ----------------- soal 1 start -----------------
+function halo() {
+  return "Halo Sanbers!";
+}
+console.log(halo());
+// ----------------- soal 1 end -----------------
+console.log("\n");
+// ----------------- soal 2 start -----------------
+function kalikan(x, y) {
+  return x * y;
+}
+var num1 = 12
+var num2 = 4
+var hasilKali = kalikan(num1, num2)
+console.log(hasilKali)
+// ----------------- soal 2 end -----------------
+console.log("\n");
+// ----------------- soal 3 start -----------------
+function introduce(name, age, address, hobby) {
+  return "Nama saya " + name + ", umur saya " + age + " tahun, alamat saya di " + address + ", dan saya punya hobby yaitu " + hobby + "!";
+}
+var name = "John"
+var age = 30
+var address = "Jalan belum jadi"
+var hobby = "Gaming"
+ 
+var perkenalan = introduce(name, age, address, hobby)
+console.log(perkenalan)
+// Menampilkan "Nama saya John, umur saya 30 tahun, alamat saya di jalan belum jadi, dan saya punya hobby yaitu Gaming!"
+// ----------------- soal 3 end -----------------
