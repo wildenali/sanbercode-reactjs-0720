f80fd8430366e785dff4ffe883c0589dbe513e7b
wildenali <wildenwildenaliali@gmail.com>
2020-07-29 08:06:31 +0700

    tugas pekan ke 2, tugas hari ke 2

--- Pekan-2/Tugas-Hari-Ke-2/index.js
+++ Pekan-2/Tugas-Hari-Ke-2/index.js
@@ -15,11 +15,24 @@ function buatObjectBook(name, timeSpent){
   return book;
 }
 
-var buku = buatObjectBook(books[0].name, books[0].timeSpent);
-console.log(buku);
+var buku0 = buatObjectBook(books[0].name, books[0].timeSpent);
+// console.log(buku0);
+
+var buku1 = buatObjectBook(books[1].name, books[1].timeSpent);
+// console.log(buku1);
+
+var buku2 = buatObjectBook(books[2].name, books[2].timeSpent);
+// console.log(buku2);
+
+var buku3 = buatObjectBook(books[3].name, books[3].timeSpent);
+// console.log(buku3);
+
 
 function sisaWaktu(waktu) {
   console.log(`Sisa waktu ${waktu}`);
 }
 
-readBooks(10000, buku, sisaWaktu);
+readBooks(10000, buku0, sisaWaktu);
+readBooks(10000, buku1, sisaWaktu);
+readBooks(10000, buku2, sisaWaktu);
+readBooks(10000, buku3, sisaWaktu);
