320ae3152bbe848694e62307bff45d420be408bf
wildenali <wildenwildenaliali@gmail.com>
2020-08-02 12:30:27 +0700

    Quiz Pekan ke-2, no-13.js

---
+++ Pekan-2/Quiz-Pekan-2-Essay/no-13.js
@@ -0,0 +1,7 @@
+let pesertaLomba = [
+  {nama: "Budi", gender: "Pria", tinggi: "172cm"},
+  {nama: "Susi", gender: "Wanita", tinggi: "162cm"},
+  {nama: "Lala", gender: "Wanita", tinggi: "155cm"},
+  {nama: "Agung", gender: "Pria", tinggi: "175cm"}
+]
+console.log(pesertaLomba);
